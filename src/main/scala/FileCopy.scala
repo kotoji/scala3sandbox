c3814366b22351f1b55a75d22c34c77c93ec4e33
kotoji <kotoji@ithnote.blue>
2024-03-18 13:45:49 +0900

    (ce3tutor) copy program F version

--- src/main/scala/FileCopy.scala
+++ src/main/scala/FileCopy.scala
@@ -7,60 +7,72 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
+import cats.effect.kernel.Sync
+import cats.effect.std.Console
+import cats.ApplicativeError
+import cats.syntax.all._
 
 object FileCopy {
-  def run(args: List[String]): IO[ExitCode] =
+  def run[F[_]: Sync: Console](
+      args: List[String]
+  )(using F: ApplicativeError[F, Throwable]): F[ExitCode] =
     for {
       _ <-
         if (args.length < 2)
-          IO.raiseError(new IllegalArgumentException("Need origin and destination files"))
+          F.raiseError(new IllegalArgumentException("Need origin and destination files")).void
         else
-          IO.unit
+          F.unit
       orig = new File(args(0))
       dest = new File(args(1))
       count <- copy(orig, dest)
-      _ <- IO.println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}")
+      _ <- Console[F].println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}")
     } yield ExitCode.Success
 
-  def inputStream(f: File): Resource[IO, FileInputStream] =
+  def inputStream[F[_]: Sync](
+      f: File
+  )(using F: ApplicativeError[F, Throwable]): Resource[F, FileInputStream] =
     Resource.make {
-      IO.blocking(new FileInputStream(f))
+      Sync[F].blocking(new FileInputStream(f))
     } { inStream =>
-      IO.blocking(inStream.close()).handleErrorWith(_ => IO.unit)
+      val close = Sync[F].blocking(inStream.close())
+      F.handleErrorWith(close)(_ => F.unit)
     }
 
-  def outputStream(f: File): Resource[IO, FileOutputStream] =
+  def outputStream[F[_]: Sync](
+      f: File
+  )(using F: ApplicativeError[F, Throwable]): Resource[F, FileOutputStream] =
     Resource.make {
-      IO.blocking(new FileOutputStream(f))
+      Sync[F].blocking(new FileOutputStream(f))
     } { outStream =>
-      IO.blocking(outStream.close()).handleErrorWith(_ => IO.unit)
+      val close = Sync[F].blocking(outStream.close())
+      F.handleErrorWith(close)(_ => F.unit)
     }
 
-  def inputOutputStreams(
+  def inputOutputStreams[F[_]: Sync](
       in: File,
       out: File
-  ): Resource[IO, (FileInputStream, FileOutputStream)] =
+  ): Resource[F, (FileInputStream, FileOutputStream)] =
     for {
       inStream <- inputStream(in)
       outStream <- outputStream(out)
     } yield (inStream, outStream)
 
-  def transfer(origin: InputStream, destination: OutputStream): IO[Long] = {
-    def go(org: InputStream, dst: OutputStream, buf: Array[Byte], acc: Long): IO[Long] =
+  def transfer[F[_]: Sync](origin: InputStream, destination: OutputStream): F[Long] = {
+    def go[F[_]: Sync](org: InputStream, dst: OutputStream, buf: Array[Byte], acc: Long): F[Long] =
       for {
-        amount <- IO.blocking(org.read(buf, 0, buf.length))
+        amount <- Sync[F].blocking(org.read(buf, 0, buf.length))
         count <-
           if (amount > -1) {
-            IO.blocking(dst.write(buf, 0, amount)) >> go(org, dst, buf, acc + amount)
+            Sync[F].blocking(dst.write(buf, 0, amount)) >> go(org, dst, buf, acc + amount)
           } else {
-            IO.pure(acc)
+            Sync[F].pure(acc)
           }
       } yield count
 
     go(origin, destination, new Array[Byte](1024 * 10), 0L)
   }
 
-  def copy(origin: File, destination: File): IO[Long] =
+  def copy[F[_]: Sync](origin: File, destination: File): F[Long] =
     inputOutputStreams(origin, destination).use { case (in, out) =>
       transfer(in, out)
     }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,7 @@
 import cats.effect.IOApp
 import cats.effect.IO
 import cats.effect.ExitCode
-import cats.effect.Resource
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.InputStream
-import java.io.OutputStream
+import cats.syntax.all._
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = FileCopy.run(args)
